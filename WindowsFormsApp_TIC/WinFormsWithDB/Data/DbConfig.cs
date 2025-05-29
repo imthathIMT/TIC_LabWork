476cf2792eb686ae03a8b8bd31c49f41b331f17c
Imthath UCA <imthath@unicornconnectedapps.com>
2025-05-29 10:59:13 +0530

    update SQL lite

---
+++ WindowsFormsApp_TIC/WinFormsWithDB/Data/DbConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsWithDB.Data
+{
+    internal static class DbConfig
+    {
+        private static string connectionString = "Data Source=UserDb.db; Version=3;";
+
+        public static SQLiteConnection GetConnection()
+        {
+            var conn = new SQLiteConnection(connectionString);
+            conn.Open();
+            return conn;
+
+        }
+    }
+}
